ec41f633266660475b37f459ddfbf4f34545fb83
EunJaePark <53172233+EunJaePark@users.noreply.github.com>
2020-04-30 11:56:59 +0900

    페이지새로접속시 이전 나라날씨보이게
    - 페이지 새롭게 접속 시 이전에 입력해놓았던 나라의 날씨 보이도록 코드 작성.
    - 함수별료 각주 작성함.

--- weather_vue/src/components/SearchCity.vue
+++ weather_vue/src/components/SearchCity.vue
@@ -23,11 +23,14 @@ export default {
     cityName() {
       console.log(`입력한 이름: ${this.cityInput}`)
       
+      // City.vue로 보내는 이벤트버스함수인 inputName()(main.js에 작성해놓은함수.)으로 전송할 데이터들 넣어줌.
+      // true는 City.vue에서 사용할 signal.
       eventbus.inputName(this.cityInput, true)
 
       // local storage에 입력한 도시이름 저장하는 함수 실행.
       this.saveName(this.cityInput)
 
+      // input입력창 다시 비워줌.
       this.cityInput = null
     },
     saveName(city) {
@@ -40,7 +43,8 @@ export default {
       const CITY_NAME = 'city_name'
       console.log(`로컬에저장할 나라이름: ${city}`);
       
-      localStorage.setItem(CITY_NAME, JSON.stringify(this.cityInput))
+      localStorage.setItem(CITY_NAME, this.cityInput)
+      // localStorage.setItem(CITY_NAME, JSON.stringify(this.cityInput))
     }
 
   }
--- weather_vue/src/views/City.vue
+++ weather_vue/src/views/City.vue
@@ -3,6 +3,12 @@
     class="city"
     
   ><!-- @cityname = "newCity" -->
+    {{ `나라이름: ${city.name}` }}
+    {{ `기온: ${city.main.temp}(섭씨로 수정해야할듯)` }}
+    {{ city.weather.main }}
+    {{ city.name }}
+
+    <br/><br/>
     {{ city }}
   </div>
 </template>
@@ -21,28 +27,40 @@ export default {
       return this.$store.state.weatherData
     }
   },
-  created() {   
+  created() {  
+    // 페이지에 새롭게 접속할 경우, 이전에 입력해놓았던 나라가 보이도록 로컬스토리지에서 나라이름 가져옴.
+    const localstorageCityName = localStorage.getItem('city_name');
+    console.log(localstorageCityName);
+    
+    this.$store.dispatch('FETCH_WEATHER', localstorageCityName);
+    console.log('aaaa');
+    
+
+    // SearchCity.vue에서 input에 입력한 나라이름 가져와서 새롭게 나라날씨 데이터 가져옴.
     eventbus.$on('sendCityName', (cityInput, signal) => {
       console.log(cityInput)
-      console.log(signal);
+      console.log(signal);     
 
+      // 나라 이름이 맞을 경우 날씨데이터 보여주고, 이름 틀릴경우 알림창 뜨도록하기.(아직 미완성)
       if(signal === true) {
         this.newCity(cityInput)
       }    
     })    
   },
   methods: {
+    // 
     newCity(cityInput) {
       console.log('city name 받았어!!!!!!!!!');
       this.$store.dispatch('FETCH_WEATHER', cityInput);
       }
     }
+}
+
 
     //1. input이 입력되면 eventBus로 보낸 city명을 받아온뒤
     //2. city명을 데이터에 저장해주고,
     //3. 이 과정을 거친 후에 store로 시티명을 보내줘야함.
-
-}
+    // 온도, 날씨정보, 도시명, 날짜 출력.
 </script>
 
 <style>
